c7ecdc2d2f75658adc7e65088672abc4b18a0c36
Zizheng Wu <me@zizhengwu.com>
2015-10-14 04:22:02 -0400

    gtmp_mcs initialization seems to be correct

--- gtmp_mcs.c
+++ gtmp_mcs.c
@@ -2,49 +2,47 @@
 #include <stdio.h>
 #include <omp.h>
 #include "gtmp.h"
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
-/*
-    From the MCS Paper: A scalable, distributed tree-based barrier with only local spinning.
 
-    type treenode = record
-        parentsense : Boolean
-	parentpointer : ^Boolean
-	childpointers : array [0..1] of ^Boolean
-	havechild : array [0..3] of Boolean
-	childnotready : array [0..3] of Boolean
-	dummy : Boolean //pseudo-data
+typedef struct _node_t{
+  bool parent_sense;
+  bool* parent_pointer;
+  bool* child_pointers[2];
+  bool have_child[4];
+  bool child_not_ready[4];
+} node_t;
 
-    shared nodes : array [0..P-1] of treenode
-        // nodes[vpid] is allocated in shared memory
-        // locally accessible to processor vpid
-    processor private vpid : integer // a unique virtual processor index
-    processor private sense : Boolean
 
-    // on processor i, sense is initially true
-    // in nodes[i]:
-    //    havechild[j] = true if 4 * i + j + 1 < P; otherwise false
-    //    parentpointer = &nodes[floor((i-1)/4].childnotready[(i-1) mod 4],
-    //        or dummy if i = 0
-    //    childpointers[0] = &nodes[2*i+1].parentsense, or &dummy if 2*i+1 >= P
-    //    childpointers[1] = &nodes[2*i+2].parentsense, or &dummy if 2*i+2 >= P
-    //    initially childnotready = havechild and parentsense = false
-	
-    procedure tree_barrier
-        with nodes[vpid] do
-	    repeat until childnotready = {false, false, false, false}
-	    childnotready := havechild //prepare for next barrier
-	    parentpointer^ := false //let parent know I'm ready
-	    // if not root, wait until my parent signals wakeup
-	    if vpid != 0
-	        repeat until parentsense = sense
-	    // signal children in wakeup tree
-	    childpointers[0]^ := sense
-	    childpointers[1]^ := sense
-	    sense := not sense
-*/
+static node_t* nodes;
+
+node_t* _gtmp_get_node(int i){
+  return &nodes[i];
+}
 
 void gtmp_init(int num_threads){
+  int num_nodes = num_threads;
+  nodes = (node_t*) malloc(num_nodes * sizeof(num_nodes));
 
+  int i;
+  for (i = 0; i < num_nodes; ++i)
+  {
+    fprintf(stderr, "initializing node %d\n", i);
+    int j;
+    for (j = 0; j < 4; ++j)
+    {
+      _gtmp_get_node(i)->have_child[j] = 4 * i + j + 1 < num_threads ? true : false;
+      fprintf(stderr, "have_child %d: %d\n", j, _gtmp_get_node(i)->have_child[j]);
+    }
+    _gtmp_get_node(i)->parent_pointer = i != 0 ? &_gtmp_get_node(floor((i-1)/4))->child_not_ready[(i-1) % 4] : NULL;
+    _gtmp_get_node(i)->child_pointers[0] = 2 * i + 1 >= num_threads ? NULL : &_gtmp_get_node(2*i+1)->parent_sense;
+    _gtmp_get_node(i)->child_pointers[1] = 2 * i + 2 >= num_threads ? NULL : &_gtmp_get_node(2*i+2)->parent_sense;
+    fprintf(stderr, "parent_pointer: %d->%d, child_pointers[0]: %d, child_pointers[1]: %d\n", (int)floor((i-1)/4), (i-1) % 4, 2 * i + 1 >= num_threads ? -1 : 2*i+1, 2 * i + 2 >= num_threads ? -1 : 2*i+2);
+    memcpy(_gtmp_get_node(i)->child_not_ready, _gtmp_get_node(i)->have_child, sizeof(_gtmp_get_node(i)->child_not_ready));
+    _gtmp_get_node(i)->parent_sense = false;
+  }
 }
 
 void gtmp_barrier(){
